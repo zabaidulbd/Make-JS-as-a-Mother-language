5372a75b391c36e3124d264eda5801f3fdfab8e5
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-11 17:07:43 +0600

    clear confusion in  declaring variable

---
+++ Chapter_Two/variable.js
@@ -0,0 +1,25 @@
+
+var userName = 'zabaidul';
+
+let age = 36;
+
+const city = 'Dhaka';
+
+console.log(userName);
+console.log(age);
+console.log(city);
+
+
+// declare a variable and assign a new value 5
+
+let number = 3;
+
+number = 5;
+
+console.log(number);
+
+let number2 = 6;
+
+number2 = 7;
+
+console.log(number2);
