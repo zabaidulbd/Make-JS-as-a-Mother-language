a7a5d54e0d8c1ca6f3a87eb13e2709d1dc9d3b88
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-15 14:45:28 +0600

    js numbers

---
+++ Chapter_Two/numbers.js
@@ -0,0 +1,32 @@
+// numbers...............
+
+let acNo = 2323;
+let acFloatNo = 23.155;
+
+console.log(acNo);
+console.log(acFloatNo);
+
+let acStNo = "12";
+
+let parseNum = Number(acStNo);
+console.log(acStNo);
+console.log(parseNum);
+
+// print the large or small number which a number can store..
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+// parse a number.......
+
+let parseNum1 = 123.123;
+
+console.log(parseInt(parseNum1));
+
+console.log(parseFloat(parseNum1));
+
+//print enfinity and NaN
+
+console.log(1 / 0);
+
+console.log("zaba" * 10);
