213d365ff8f5ba14e4b70f3b85056623f4477388
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-15 23:08:30 +0600

    type conversion

---
+++ Chapter_Two/typeconversion.js
@@ -0,0 +1,20 @@
+// String to Number
+let age = "18";
+let ageNumber = Number(age); // 18 (number)
+let ageInt = parseInt(age); // 18
+let ageFloat = parseFloat("18.5"); // 18.5
+
+// Number to String
+let price = 500;
+let priceText = String(price); // "500"
+let priceText2 = price.toString(); // "500"
+
+// To Boolean
+let value1 = Boolean(1); // true
+let value2 = Boolean(0); // false
+let value3 = Boolean("hi"); // true
+
+// Implicit conversion
+let result1 = "5" + 3; // "53" (string)
+let result2 = "5" - 3; // 2 (number)
+let result3 = "5" * "2"; // 10 (number)
