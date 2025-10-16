116340753c8acfadf67d8c2931b68a43f0542570
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-16 15:10:45 +0600

    use math in different purposes

---
+++ Chapter_Two/math.js
@@ -0,0 +1,21 @@
+// math in js...........
+
+// const value........
+
+console.log(Math.E);
+console.log(Math.PI);
+
+// other functions............
+
+console.log(Math.pow(2, 3));
+console.log(Math.pow(3, 2));
+
+console.log(Math.sqrt(9));
+
+console.log(Math.max(2, 4, 6));
+
+console.log(Math.min(2, 3, 7));
+console.log(Math.floor(4.23));
+console.log(Math.ceil(4.23));
+console.log(Math.round(4.23));
+console.log(Math.round(Math.random() * 50 + 1));
