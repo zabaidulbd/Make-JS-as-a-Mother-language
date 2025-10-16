9e2900442b9dfc4e29a8f0f515990716ce6cd2ef
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-16 16:25:04 +0600

    about dates

---
+++ Chapter_Two/date.js
@@ -0,0 +1,11 @@
+let dateToday = new Date();
+
+console.log(dateToday.toDateString());
+console.log(dateToday.toTimeString());
+console.log(dateToday.toLocaleString());
+console.log(dateToday.getFullYear());
+console.log(dateToday.getMonth());
+console.log(dateToday.getDate());
+console.log(dateToday.getHours());
+console.log(dateToday.getMinutes());
+console.log(dateToday.getSeconds());
