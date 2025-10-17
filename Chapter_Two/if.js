a1f4b56431361e52980bf2b93662a20974824c00
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-17 10:51:00 +0600

    if else condition

---
+++ Chapter_Two/if.js
@@ -0,0 +1,25 @@
+//if conditions..........
+
+let age = 18;
+
+if (age >= 18) {
+  console.log("you are able to vote");
+}
+
+let tempToday = 20;
+
+if (tempToday <= 19) {
+  console.log("its too cold today");
+}
+
+// if else conditions........if we confirm that we have only two value
+
+let broAge = 18;
+
+let myAge = 20;
+
+if (myAge >= broAge) {
+  console.log("I am elder than him");
+} else {
+  console.log("we are same ");
+}
