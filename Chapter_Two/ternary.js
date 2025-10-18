b80c6e9ab5d826bd7680b6c5617076aa1eb51294
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-18 17:04:23 +0600

    add ternary opeartor

---
+++ Chapter_Two/ternary.js
@@ -0,0 +1,15 @@
+//ternary opearator........
+
+// formate:  condition ? true : false
+
+let babaAge = 2;
+
+let babaOutputAge =
+  babaAge > 1 ? "baba is grow older day by day" : "baba is too small";
+
+console.log(babaOutputAge);
+
+const marks = 70;
+
+const totalMarks = marks >= 70 ? "you will get A grade" : "you obtain garde A-";
+console.log(totalMarks);
