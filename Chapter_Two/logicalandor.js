505b88bf0c25769e21ec6ab60e632e957de8ac4e
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-18 17:30:11 +0600

    logical and or

---
+++ Chapter_Two/logicalandor.js
@@ -0,0 +1,22 @@
+// logical and and logiacal or.....
+
+let name = "";
+
+let fullName = name || console.log("Zabaidul");
+console.log(fullName);
+
+let name1 = "Mona";
+
+let fullName1 = name1 || console.log("Zabaidul");
+console.log(fullName1);
+
+// logical &&......
+
+let hasNid = true;
+
+hasNid && console.log("He has nid");
+
+let hasNid1 = false;
+
+let result1 = hasNid1 && console.log("He has nid");
+console.log(result1);
