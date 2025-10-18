7d3ad2607c31f95750735cce3f8b0e9c96dc7b0f
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-10-18 15:04:47 +0600

    add logical opeartors

---
+++ Chapter_Two/logicaloperator.js
@@ -0,0 +1,33 @@
+// logical operators.....if two conditon is true return true....
+
+let age = 20;
+
+let hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+  console.log("you are able to drive");
+} else {
+  console.log("you are unalble to drive a car");
+}
+
+let age2 = 17;
+
+let hasLicense2 = true;
+
+if (age2 >= 18 && hasLicense2) {
+  console.log("you are able to drive");
+} else {
+  console.log("you are unalble to drive a car");
+}
+
+// or || operator....if one conditon is true return true......
+
+let papaAge = 1;
+
+let hasInjection = false;
+
+if (papaAge >= 2 || hasInjection) {
+  console.log("papa is save from fever");
+} else {
+  console.log("he is in trouble");
+}
